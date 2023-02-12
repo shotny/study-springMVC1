c2467ff4694ed7b91cb72e8b699b1589a88956f2
shotny <yeahh2483@gmail.com>
2023-02-12 22:37:29 +0900

    스프링MVC 구조_핸들러매핑 & 핸들러어댑터

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller") //컴포넌트스캔_빈이름을 url 패턴으로 한것, 핸들러 매핑이 스프링 빈 이름으로 핸들러 찾았움
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
